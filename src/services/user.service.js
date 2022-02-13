1d34f9b13768324799d26da9ef7a1a9b26bb2ab3
Christos Baloukas <cmpalouk@me.com>
2022-02-13 13:17:07 +0200

    added new hostname paths

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,6 +8,8 @@ class UserService {
   constructor() {
     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
       this.API_URL = 'http://localhost:8080/api/test/';
+    } else if (window.location.hostname === 'ntua-app-v1') {
+      this.API_URL = 'https://ntua-node-auth:8443/api/auth/';
     } else {
       this.API_URL = 'http://cndevs.cn.ntua.gr:8080/api/test/';
     }
