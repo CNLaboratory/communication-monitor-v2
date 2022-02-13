5d9ab9ee82cd4b2ecc93c6a6f347d06eb10ed193
Christos Baloukas <cmpalouk@me.com>
2022-02-13 14:07:55 +0200

    fixed hostnames

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,8 +7,8 @@ class AuthService {
   constructor() {
     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
       this.API_URL = 'http://localhost:8080/api/auth/';
-    } else if (window.location.hostname === 'ntua-app-v1') {
-      this.API_URL = 'https://ntua-node-auth:8443/api/auth/';
+    } else if (window.location.hostname === 'ntua-app-v1.codeheaven.gr') {
+      this.API_URL = 'https://ntua-node.codeheaven.gr:8443/api/auth/';
     } else {
       this.API_URL = 'http://cndevs.cn.ntua.gr:8080/api/auth/';
     }
